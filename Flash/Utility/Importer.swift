88ea9eb29d1ba0e0c9477bf37cd5c560ed514c34
John Forde <fordee@me.com>
2019-03-27 06:38:31 +1300

    Added deck and card delete functionality.

---
+++ Flash/Utility/Importer.swift
@@ -0,0 +1,9 @@
+//
+//  Importer.swift
+//  Flash
+//
+//  Created by John Forde on 26/03/19.
+//  Copyright © 2019 4DWare. All rights reserved.
+//
+
+import Foundation
---
+++ Flash/Utility/TemporaryFile.swift
@@ -0,0 +1,9 @@
+//
+//  TemporaryFile.swift
+//  Flash
+//
+//  Created by John Forde on 26/03/19.
+//  Copyright © 2019 4DWare. All rights reserved.
+//
+
+import Foundation
